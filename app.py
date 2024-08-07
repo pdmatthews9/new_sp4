f9f8ada6acdddae53632b9336b290276bfacd6ad
Paris Matthews <p.matthews985@gmail.com>
2024-08-07 15:22:28 -0500

    Edits

--- app.py
+++ app.py
@@ -6,12 +6,11 @@ df = pd.read_csv('vehicles_us.csv')
 
 st.header('Vehicle Data and Analytics')
 
-histfig = px.histogram(df, x='type', y='price', title= 'Popular Vehicle Types')
+histfig = px.histogram(df, x='type', y='price', title= 'Most Valuable Vehicle Type', labels={'type': 'Vehicle Type', 'price': 'Vehicle Price ($)'})
 st.plotly_chart(histfig)
 
-scattfig = px.scatter(df, x='odometer', y='price', title= 'Odometer vs. Price')
+scattfig = px.scatter(df, x='odometer', y='price', title= 'Odometer and Price Correlation', labels= {'odometer': 'Odometer/ Mileage', 'price': 'Vehicle Cost ($)'})
 st.plotly_chart(scattfig)
 
 if st.checkbox('Display Scatter Plot'):
     st.plotly_chart(scattfig)
-    
